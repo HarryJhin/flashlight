d234c1afa8f32762dbf0458fd6a9a19b1d3810d3
jjh <joojinhyun00@gmail.com>
2022-05-15 20:53:06 +0900

    torch 서비스 추가

---
+++ app/src/main/java/blog/jinhyun/flashlight/TorchService.kt
@@ -0,0 +1,12 @@
+package blog.jinhyun.flashlight
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TorchService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
